661411046d5504f0e8fefc7fbe8fa0d53eef3d16
Andres Gasca <afgc.indigo@gmail.com>
2020-04-27 17:55:03 -0500

    I modified the script in order to find lines in a silo log. For example, searching for the word 'ERROR'. The script will show you all the coincidences

--- SearchWordIntoFile.py
+++ SearchWordIntoFile.py
@@ -10,16 +10,12 @@ def main():
 
 def search_word_into_file(file_path, word_to_search):
     with open(file_path, 'r') as file:
-        words = file.read().replace('\n', '\n ').split(' ')
+        words = file.read().split('\n')
         occurrences = 0
         matched_words = ''
         for x in range(len(words)):
             if word_to_search in words[x]:
-                occurrences += 1
-                matched_words += str(x) + ','
-        print(f'The "{file_path}" file has {len(words)} words.')
-        print(f'The word "{word_to_search}" is present in {occurrences} of the {len(words)} words in the file')
-        print(f'Matched words indexes of the word "{word_to_search}": {matched_words}')
+                print(words[x])
 
 
 if __name__ == '__main__':
